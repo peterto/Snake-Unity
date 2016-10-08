1ab649a1f4b386706e2a019fa29c309e53e611ee
Peter <pt9386@gmail.com>
2016-10-07 20:48:44 -0400

    Initial commit

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+
+	private Vector2 _dir = Vector2.right;
+	private List<Transform> _tail = new List<Transform> ();
+	private bool _ate = false;
+
+	public GameObject _tailPrefab;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Move", 0.3f, 0.3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.RightArrow))
+			_dir = Vector2.right;
+		else if (Input.GetKey (KeyCode.DownArrow))
+			_dir = -Vector2.up;
+		else if (Input.GetKey (KeyCode.UpArrow))
+			_dir = Vector2.up;
+		else if (Input.GetKey (KeyCode.LeftArrow))
+			_dir = -Vector2.right;
+	
+	}
+
+	void Move() {
+		Vector2 newVector = transform.position;
+		transform.Translate (_dir);
+
+		if (_ate) {
+			GameObject gameObject = (GameObject)Instantiate (_tailPrefab, newVector, Quaternion.identity);
+
+			_tail.Insert (0, gameObject.transform);
+
+			_ate = false;
+		} else if (_tail.Count > 0) {
+			_tail.Last().position = newVector;
+			_tail.Insert (0, _tail.Last());
+			_tail.RemoveAt (_tail.Count - 1);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.name.StartsWith ("food")) {
+			_ate = true;
+			Destroy (col.gameObject);
+		} else {
+			
+		}
+
+	}
+}
---
+++ Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour {
+
+	public GameObject _food;
+
+	public Transform _borderTop;
+	public Transform _borderBottom;
+	public Transform _borderLeft;
+	public Transform _borderRight;
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating ("Spawn", 3, 4);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Spawn() {
+		int x = (int)Random.Range (_borderLeft.position.x, _borderRight.position.x);
+		int y = (int)Random.Range (_borderBottom.position.y, _borderTop.position.y);
+
+		Instantiate (_food, new Vector2 (x, y), Quaternion.identity);
+	}
+}
